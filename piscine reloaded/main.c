09549b255a9dd14ab6c5557fcf61bc859506bff3
Diego Vargas <divaro_@hotmail.com>
2025-09-24 11:43:36 +0200

    Initial Commit

---
+++ piscine reloaded/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  piscine reloaded
+//
+//  Created by Diego Vargas on 24/9/25.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
