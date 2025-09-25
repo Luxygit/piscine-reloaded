721b9841995a3341d0e414049c87dde27a674132
Diego Vargas <divaro_@hotmail.com>
2025-09-25 18:20:12 +0200

    ex10 done

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dievarga <dievarga@student.42barcelona.co  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/25 10:11:12 by dievarga          #+#    #+#             */
+/*   Updated: 2025/09/25 11:33:16 by dievarga         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+	{
+		ft_putchar('N');
+	}
+	else
+	{
+		ft_putchar ('P');
+	}
+}
+
+/*
+int	main(void)
+{
+	ft_is_negative(-2);
+	ft_putchar('\n');
+	ft_is_negative(0);
+	ft_putchar('\n');
+	ft_is_negative(2);
+	ft_putchar('\n');
+	return (0);
+}
+*/
